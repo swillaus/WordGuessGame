af2dfae6a62821aedf3b011e36ad8f3a062ba09b
Your Name <sam@upsidebusinessgroup.com.au>
2019-08-02 10:11:41 -0400

    commit basic HTML & CSS template

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+<script>
+
+{/* create an array with all potential guesses */}
+
+{/* create a loop that randomally picks one of the values in that array */}
+
+{/* create variables to hold user inputs */}
+
+{/* check user inputs and whether that letter is contained in the string */}
+
+{/* Create a variable that logs the users guesses */}
+
+{/* Create a variable for the number of wins  */}
+
+
+
+</script>
